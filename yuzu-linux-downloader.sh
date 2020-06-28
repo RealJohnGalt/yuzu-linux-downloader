fb1fb3751f0f157e8c7ad1ffab0badb2b8e16698
Chris Gresock <chris.gresock@gmail.com>
2020-06-28 15:06:08 -0400

    added symlink switch to create fixed build locations

--- yuzu-linux-downloader.sh
+++ yuzu-linux-downloader.sh
@@ -28,7 +28,7 @@ if [ -x qmake ]; then
     exit 1
 fi
 
-while getopts ":c:d:hgolf" options; do
+while getopts ":c:d:hgolfs" options; do
     case "${options}" in
         h) usage; exit 0;;        
         c) CHANNEL=${OPTARG};;
@@ -39,6 +39,7 @@ while getopts ":c:d:hgolf" options; do
         o) opts=1;;
         l) clangbuild=1;;
         w) webengine=1;;
+        s) symlinkbuild=1;;
         :)
             echo "Error: -${OPTARG} requires an argument or invalid option."
             exit_abnormal
@@ -152,5 +153,12 @@ else
 fi
 PATH="/usr/lib/ccache/bin/:$PATH" make -j$(($(nproc) -1))
 bindir="$(pwd -L)/bin"
+
+if [[ "$symlinkbuild" == "1" ]]; then
+    cd ../..
+    symdir="$(pwd -L)/$CHANNEL"
+    echo "Symlinking binaries to build target. Latest binaries should be in $symdir"
+    ln -s "$bindir" "$symdir"
+fi
 
 echo "Your build should be in $bindir"
